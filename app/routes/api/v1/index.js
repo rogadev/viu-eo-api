ede3210d09cbf5f80573d067814156eb27e68579
vonroga <ryanroga@gmail.com>
2022-06-24 12:33:05 -0700

    Update index.js

--- app/routes/api/v1/index.js
+++ app/routes/api/v1/index.js
@@ -1,9 +1,12 @@
 module.exports = (app) => {
+  // Express Router
   const router = require('express').Router()
 
-  router.get('/test', (req, res) => {
+  // GET /api/v1/test - Test route - Route linked to in console when server starts.
+  router.get('/test', (_, res) => {
     res.send('Hello World!')
   })
 
+  // Connect routes in /api/v1/ to the Express app.
   app.use('/api/v1/', router)
 }
