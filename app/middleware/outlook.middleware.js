e980ab9ea727d390484e461578956d7d648b2d1a
vonroga <ryanroga@gmail.com>
2022-06-29 13:01:44 -0700

    Create outlook.middleware.js

---
+++ app/middleware/outlook.middleware.js
@@ -0,0 +1,32 @@
+exports.requiresNocParam = function (req, res, next) {
+  // noc should be a string that contains 4 characters, each character being a number
+  if (!req.params.noc) {
+    return res.status(400).send('Missing noc parameter')
+  }
+  const noc = req.params.noc
+  if (noc.length !== 4) {
+    return res.status(400).send('Invalid noc parameter (length)')
+  }
+  if (new RegExp(/^[0-9]*$/gm).test(noc) === false) {
+    return res.status(400).send('Invalid noc parameter (format)')
+  }
+  const validCodes = require('../data/noc/2016/noc_2016_noc_codes.json')
+  if (!validCodes.includes(noc)) {
+    return res.status(400).send('Invalid noc parameter (value)')
+  }
+  next()
+}
+
+exports.requiresRegionQuery = function (req, res, next) {
+  if (!req.query.region) {
+    res.status(400).send('Missing required parameters (region)')
+  }
+  next()
+}
+
+exports.requiresProvinceQuery = function (req, res, next) {
+  if (!req.query.province) {
+    res.status(400).send('Missing required parameters (province)')
+  }
+  next()
+}
