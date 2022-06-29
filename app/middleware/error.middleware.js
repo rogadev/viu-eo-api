856653172500e5bef20b11b62dce20b760bd0f78
vonroga <ryanroga@gmail.com>
2022-06-29 13:01:34 -0700

    Update error.middleware.js

--- app/middleware/error.middleware.js
+++ app/middleware/error.middleware.js
@@ -1,4 +1,10 @@
-exports = (err, req, res, next) => {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
+module.exports = (error, req, res, next) => {
+  console.log('Error Handling Middleware called')
+  console.log('Path: ', req.path)
+  console.error('Error: ', error)
+  if (error.type == 'redirect') res.redirect('/error')
+  else if (error.type == 'time-out')
+    // arbitrary condition check
+    res.status(408).send(error)
+  else res.status(500).send(error)
 }
