ba8a5b0d7e837465f0c075678f1d799f2248d110
vonroga <ryanroga@gmail.com>
2022-06-29 12:59:37 -0700

    Update outlook.controller.js
    Extracted middleware functions into their own file.

--- app/controllers/outlook.controller.js
+++ app/controllers/outlook.controller.js
@@ -1,22 +1,10 @@
-function checkForRequiredParams(res, ...args) {
-  if (!args.length) {
-    res.status(400).send('Missing required parameters')
-  }
-  args.forEach((arg) => {
-    if (!arg) {
-      res.status(400).send('Missing required parameters')
-    }
-  })
-}
-
 exports.nationalOutlook = function (req, res) {
   const noc = req.params.noc
   // example query: GET https://lmi-outlooks-esdc-edsc-apicast-production.api.canada.ca/clmix-wsx/gcapis/outlooks/ca?noc=1111
+  res.send(noc)
 }
 
 exports.provincialOutlook = function (req, res) {
-  checkForRequiredParams(res, req.query.noc, req.query.region)
-
   const noc = req.query.noc
   const rpt = 1 // 1 = Provincial/Territorial, 2 = Economic Region
   const rgn = req.query.region || 49
@@ -25,10 +13,8 @@ exports.provincialOutlook = function (req, res) {
 }
 
 exports.regionalOutlook = function (req, res) {
-  checkForRequiredParams(res, req.query.noc, req.query.region)
-
   const noc = req.query.noc
-  const rpt = 2 // 1 = Provincial/Territorial, 2 = Economic Region
+  const rpt = 2 // 2 = Economic Region, 1 = Provincial/Territorial
   const rgn = req.query.region
   const lang = 'en'
   // example query: GET https://lmi-outlooks-esdc-edsc-apicast-production.api.canada.ca/clmix-wsx/gcapis/outlooks?noc=1111&rtp=1&rid=10
