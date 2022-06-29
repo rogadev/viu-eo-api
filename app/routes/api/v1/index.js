008072b8f853616cb58d2943a9295e52afdafa8b
vonroga <ryanroga@gmail.com>
2022-06-29 15:31:04 -0700

    Update API Routes

--- app/routes/api/v1/index.js
+++ app/routes/api/v1/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router()
 
 // MIDDLEWARE
 const outlookMW = require('../../../middleware/outlook.middleware.js')
+const programMW = require('../../../middleware/program.middleware.js')
 
 // CONTROLLERS
 const test = require('../../../controllers/test.controller.js')
@@ -12,20 +13,34 @@ const outlooks = require('../../../controllers/outlook.controller.js')
 // GET /api/v1/test - Test route.
 router.get('/test', test.test)
 
-// GET /api/v1/programs/searchable - Returns list of all searchable programs.
-router.get('/programs/searchable', programs.findSearchable)
-
-// GET /api/v1/programs - Returns list of all programs offered at VIU.
-router.get('/programs', programs.findAll)
+// GET /api/v1/area/:nid - Returns area based on its NID.
+router.get(
+  '/area/:nid',
+  programMW.requiresNidParam,
+  programMW.validateAreaNidParam,
+  programs.findArea
+)
 
 // GET /api/v1/programs/:nid - Returns program based on its NID.
-router.get('/program/:nid', programs.findOne)
-
-// GET /api/v1/programs/area/:nid - Returns area based on its NID.
-router.get('/program/area/:nid', programs.findArea)
+router.get(
+  '/program/:nid',
+  programMW.requiresNidParam,
+  programMW.validateProgramNidParam,
+  programs.findOne
+)
 
 // GET /api/v1/programs/area/:nid - Returns area based on program NID.
-router.get('/program/area/:nid', programs.findAreaByProgram)
+router.get(
+  '/program/area/:nid',
+  programMW.requiresNidParam,
+  programs.findAreaByProgram
+)
+
+// GET /api/v1/programs - Returns list of all programs offered at VIU.
+router.get('/programs/all', programs.findAll)
+
+// GET /api/v1/programs/searchable - Returns list of all searchable programs.
+router.get('/programs/searchable', programs.findSearchable)
 
 // GET /api/v1/national/outlook/:noc - Returns area based on program NID.
 router.get(
