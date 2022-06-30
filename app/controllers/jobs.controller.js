955cef6cc9e184e814f40adcdefd30d915a98a1d
vonroga <ryanroga@gmail.com>
2022-06-30 10:46:42 -0700

    Update jobs.controller.js
    Stubbed for testing.

--- app/controllers/jobs.controller.js
+++ app/controllers/jobs.controller.js
@@ -2,4 +2,6 @@ const unitGroups = require('../data/noc/2016/noc_2016_unit_groups.json')
 
 exports.jobsByCredential = function (req, res) {}
 
-exports.jobsByCredential = function (req, res) {}
+exports.jobsByProgram = function (req, res) {
+  res.send(req.params.nid)
+}
