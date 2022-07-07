6a97084790a750e2678ab0b7f4f5fbc51013ad87
Roga <ryanroga@gmail.com>
2022-07-07 12:52:48 -0700

    Update groups.controller.js
    Added array helper. Adds get multiple groups controller function.

--- app/controllers/groups.controller.js
+++ app/controllers/groups.controller.js
@@ -1,4 +1,8 @@
+// DATA
 const unitGroups = require('../data/noc/2016/noc_2016_unit_groups.json')
+
+// HELPERS
+const { ensureArray } = require('../helpers/array.helpers.js')
 
 /**
  * Get one unit group, based on NOC, passed as a param.
@@ -12,4 +16,20 @@ exports.getGroup = (req, res) => {
     res.json(result)
   }
   res.status(500).send('Something went wrong.')
+}
+
+/**
+ * Get all unit groups, based on NOC, passed in as query string.
+ * @query nocs Array or list of NOC codes to return. Searches NOC 2016 V1.3 as of July 7, 2022
+ * @returns list of NOC codes
+ */
+exports.getGroups = (req, res) => {
+  const nocs = ensureArray(req.query.nocs)
+  const results = unitGroups.reduce((acc, curr) => {
+    const result = nocs.includes(curr.noc)
+    if (result) return [...acc, curr]
+    return [...acc]
+  }, [])
+  if (results) res.json(results)
+  res.status(500).send('Something went wrong')
 }
