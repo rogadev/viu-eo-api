72cc0ac9635abcdf27896a835279242f5f397019
vonroga <ryanroga@gmail.com>
2022-07-20 11:42:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ App.use(express.json())
 // FALLBACK ROUTE
 App.get('/', (req, res) =>
   res.send(`
-/api/v1/test - Test route.\n
-/api/v1/fix - Data Fixing Route (mothballed).\n
-/api/v1/area/:nid - Returns area based on its NID.\n
-/api/v1/program/:nid - Returns program based on its NID.\n
-/api/v1/program/area/:nid - Returns area based on program NID.\n
-/api/v1/programs - Returns list of all programs offered at VIU.\n
-/api/v1/programs/searchable - Returns list of all searchable programs.\n
-/api/v1/jobs/program/:nid - Returns list of all searchable programs.\n
-/api/v1/jobs/credential - Returns list of jobs based on a given credential and keywords query.\n
+<a href="/api/v1/test">/api/v1/test</a> - Test route.<br>
+<a href="/api/v1/fix">/api/v1/fix</a> - Data Fixing Route (mothballed).<br>
+<a href="/api/v1/area/9143">/api/v1/area/:nid</a> - Returns area based on its NID.<br>
+<a href="/api/v1/program/3">/api/v1/program/:nid</a> - Returns program based on its NID.<br>
+<a href="/api/v1/program/area/9143">/api/v1/program/area/:nid</a> - Returns area based on program NID.<br>
+<a href="/api/v1/programs">/api/v1/programs</a> - Returns list of all programs offered at VIU.<br>
+<a href="/api/v1/programs/searchable">/api/v1/programs/searchable</a> - Returns list of all searchable programs.<br>
+<a href="/api/v1/jobs/program/9143">/api/v1/jobs/program/:nid</a> - Returns list of jobs based on a given NID param.<br>
+<a href="/api/v1/jobs/credential">/api/v1/jobs/credential</a> - Returns list of jobs based on a given credential and keywords query.<br>
 `)
 )
 
