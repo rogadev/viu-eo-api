05a0cd713bf38c8eb46cf782c7f83a70bffec48e
vonroga <ryanroga@gmail.com>
2022-07-26 15:08:58 -0700

    Update extract_jobs.helper.js
    Imported the titleCase helper. Hard coded 2016 data because dynamic importing was causing problems. Removed ability to select version of NOC codes via parameter (with default value). Corrected and simplified logic. Updated for newer data format when returning a list of jobs.

--- app/helpers/extract_jobs.helper.js
+++ app/helpers/extract_jobs.helper.js
@@ -1,35 +1,27 @@
 // HELPERS
 const { pushIfUnique, ensureArray } = require('./array.helpers.js')
+const { titleCase } = require('./string.helpers.js')
+
+const unitGroups = require('../data/noc/2016/noc_2016_unit_groups.json')
 
 /**
  * Extracts jobs from a given version of NOC unit groups (2016 or 2021), for given a set of NOC numbers.
- * @param {Array} arr - An array of NOC unit group numbers as strings.
- * @param {Number} nocVersion - The NOC version to use. 2016 by default. 2021 also available.
+ * @param {Array<String>} arr - An array of NOC unit group numbers as strings.
  * @returns An array of jobs item objects - ex. [{noc: 1234, titles: ['','','',...]},{...},{...},...]
  */
-module.exports = (arr, nocVersion = 2016) => {
+module.exports = (arr) => {
+  const jobResults = []
   const unitGroupsArray = ensureArray([...arr])
-  const unitGroups =
-    nocVersion === 2021
-      ? require('../data/noc/2021/noc_2021_unit_groups.json')
-      : require('../data/noc/2016/noc_2016_unit_groups.json')
 
-  const jobs = []
+  for (const noc of unitGroupsArray) {
+    const { jobs } = unitGroups.find((unitGroup) => unitGroup.noc === noc)
 
-  for (const unitGroup of unitGroups) {
-    if (unitGroupsArray.includes(unitGroup.noc)) {
-      const jobs = unitGroup.jobs
-
-      const item = {
-        noc: unitGroup.noc,
-        titles: jobs,
-      }
-
-      pushIfUnique(jobs, item)
-    }
+    jobs.forEach((job) => {
+      pushIfUnique(jobResults, { noc, title: titleCase(job) })
+    })
   }
 
-  return jobs
+  return jobResults
 }
 
 // TODO - test this function!
