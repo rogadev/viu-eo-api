79b98946339d2881b4e5cd4c594593f2aac27941
vonroga <ryanroga@gmail.com>
2022-07-28 14:22:46 -0700

    Mothball fix.controller.js
    Safely handles mothballing route.

--- app/controllers/fix.controller.js
+++ app/controllers/fix.controller.js
@@ -3,51 +3,53 @@ const path = require('path')
 const oldData = require('../data/noc/2016/noc_2016_unit_groups.json')
 
 exports.fixData = (req, res) => {
-  const collector = []
-  for (const unitGroup of oldData) {
-    const jobsSection = unitGroup.sections.find((section) =>
-      section.title.includes('Illustrative example(s)')
-    )
-    const exclusionsSection = unitGroup.sections.find((section) =>
-      section.title.includes('Exclusion(s)')
-    )
-    const requirementsSection = unitGroup.sections.find((section) =>
-      section.title.includes('Employment requirements')
-    )
+  res.send('This route is no longer in use.')
 
-    const requirements = requirementsSection.items.every(
-      (el) => typeof el === 'string'
-    )
-      ? requirementsSection.items.map((item) => '' + item)
-      : requirementsSection.items.map((item) =>
-          item.items.map((subItem) => '' + subItem)
-        )
+  // const collector = []
+  // for (const unitGroup of oldData) {
+  //   const jobsSection = unitGroup.sections.find((section) =>
+  //     section.title.includes('Illustrative example(s)')
+  //   )
+  //   const exclusionsSection = unitGroup.sections.find((section) =>
+  //     section.title.includes('Exclusion(s)')
+  //   )
+  //   const requirementsSection = unitGroup.sections.find((section) =>
+  //     section.title.includes('Employment requirements')
+  //   )
 
-    const fixedRequirements = requirements.map((item) => {
-      return item
-        .toString()
-        .replace(/\s+/g, ' ')
-        .replace(/[\r\n]/g, '')
-        .trim()
-    })
+  //   const requirements = requirementsSection.items.every(
+  //     (el) => typeof el === 'string'
+  //   )
+  //     ? requirementsSection.items.map((item) => '' + item)
+  //     : requirementsSection.items.map((item) =>
+  //         item.items.map((subItem) => '' + subItem)
+  //       )
 
-    const dutiesSection = unitGroup.sections.find((section) =>
-      section.title.includes('Main duties')
-    )
-    const newUnitGroup = {
-      noc: unitGroup.noc,
-      title: unitGroup.title,
-      jobs: jobsSection.items,
-      exclusions: exclusionsSection.items,
-      requirements: fixedRequirements,
-      duties: dutiesSection.items,
-    }
-    collector.push(newUnitGroup)
-  }
+  //   const fixedRequirements = requirements.map((item) => {
+  //     return item
+  //       .toString()
+  //       .replace(/\s+/g, ' ')
+  //       .replace(/[\r\n]/g, '')
+  //       .trim()
+  //   })
 
-  fs.writeFileSync(
-    path.join(__dirname, '../data/noc/2016/new_noc_2016_unit_groups.json'),
-    JSON.stringify(collector, null, 2)
-  )
-  res.send('Done')
+  //   const dutiesSection = unitGroup.sections.find((section) =>
+  //     section.title.includes('Main duties')
+  //   )
+  //   const newUnitGroup = {
+  //     noc: unitGroup.noc,
+  //     title: unitGroup.title,
+  //     jobs: jobsSection.items,
+  //     exclusions: exclusionsSection.items,
+  //     requirements: fixedRequirements,
+  //     duties: dutiesSection.items,
+  //   }
+  //   collector.push(newUnitGroup)
+  // }
+
+  // fs.writeFileSync(
+  //   path.join(__dirname, '../data/noc/2016/new_noc_2016_unit_groups.json'),
+  //   JSON.stringify(collector, null, 2)
+  // )
+  // res.send('Done')
 }
