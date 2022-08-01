15a45cd175ea2d68fb868f1d0555680f56d21579
vonroga <ryanroga@gmail.com>
2022-08-01 12:31:21 -0700

    Update helpers.test.js
    Attempting to get test to work. Having an issue with jest not importing describe, expect, it, yet it's not throwing an error of any kind. 🤷‍♂️ I've put a few posts out to my network - waiting to see what comes back.

--- app/__tests__/helpers.test.js
+++ app/__tests__/helpers.test.js
@@ -1,19 +1,18 @@
-const { describe, expect, it } = require('jest')
-
-const requestWithSupertest = require('../config/tests.config.js')
+const { describe, expect, test } = require('jest')
+// const requestWithSupertest = require('../config/tests.config.js')
 const arrayHelpers = require('../helpers/array.helpers.js')
 
 describe('Array Helpers', () => {
   describe('pushIfUnique()', () => {
-    it('Pushes if unique', () => {
+    test('Pushes if unique', () => {
       const arr = [1, 2, 3]
       arrayHelpers.pushIfUnique(arr, 4)
       expect(arr).toEqual([1, 2, 3, 4])
-    })
-    it('Does not push if duplicate', () => {
+    }, 30000)
+    test('Does not push if duplicate', () => {
       const arr = [1, 2, 3]
       arrayHelpers.pushIfUnique(arr, 3)
       expect(arr).toEqual([1, 2, 3])
-    })
+    }, 30000)
   })
 })
