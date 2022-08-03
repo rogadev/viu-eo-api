5138712c2e4b9560869d1121d7fe579480048354
vonroga <ryanroga@gmail.com>
2022-08-03 12:50:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const MainRouter = require('./app/routes')
 const cors = require('cors')
 App.use(express.json())
 App.use(cors())
+// TODO may need to fix this. Still blocking cross-origin requests.
 
 // FALLBACK ROUTE
 App.get('/', (req, res) =>
