9f593b25cfe393b501d8efdb4e524914e155f94e
Roga <ryanroga@gmail.com>
2022-08-08 09:32:42 -0700

    Update index.js
    Fixed issue. Had renamed this route to add /all to display all programs.

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ App.get('/', (req, res) =>
 <a href="/api/v1/area/3">/api/v1/area/:nid</a> - Returns area based on its NID.<br>
 <a href="/api/v1/program/9143">/api/v1/program/:nid</a> - Returns program based on its NID.<br>
 <a href="/api/v1/program/area/9143">/api/v1/program/area/:nid</a> - Returns area based on program NID.<br>
-<a href="/api/v1/programs">/api/v1/programs</a> - Returns list of all programs offered at VIU.<br>
+<a href="/api/v1/programs/all">/api/v1/programs/all</a> - Returns list of all programs offered at VIU.<br>
 <a href="/api/v1/programs/searchable">/api/v1/programs/searchable</a> - Returns list of all searchable programs.<br>
 <a href="/api/v1/jobs/program/9143">/api/v1/jobs/program/:nid</a> - Returns list of jobs based on a given NID param.<br>
 <a href="/api/v1/jobs-by-credential?credential=degree&keywords=computer+science">/api/v1/jobs-by-credential</a> - Returns list of jobs based on a given credential and keywords query.<br>
