abee77364cec6f7da21834cf62fde8e04c0acb57
vonroga <ryanroga@gmail.com>
2022-08-19 10:48:42 -0700

    Update array.helpers.js
    Refactor for use of named functions inside of other exports. Adds keywordCombinator() as a helper function.

--- app/helpers/array.helpers.js
+++ app/helpers/array.helpers.js
@@ -3,7 +3,7 @@
  * @param {Array} arr - The array where we will try to put our item.
  * @param {*} item - The item we want to put in the array.
  */
-exports.pushIfUnique = (arr, item) => {
+const pushIfUnique = (arr, item) => {
   // First, ensure that our array is, in fact, an array.
   if (!Array.isArray(arr)) {
     throw new Error(
@@ -20,13 +20,29 @@ exports.pushIfUnique = (arr, item) => {
 /**
  * Ensures that the given input is an array. If not, input is converted into an array.
  * @param {Array | string} input - Accepts either an array or a string. If a string, it is converted into an array. If the string contains `,` it is split by commas and separated into an array.
- * @returns an array.
+ * @returns {string[]} an array.
  */
-exports.ensureArray = (input) => {
+const ensureArray = (input) => {
   if (Array.isArray(input)) {
     return input
   } else {
     let arr = input.split(',')
     return arr.map((item) => item.trim())
   }
+}
+
+const keywordCombinator = (arr1, arr2) => {
+  const combined = []
+  arr1.forEach((item1) => {
+    arr2.forEach((item2) => {
+      pushIfUnique([item1.toLowerCase(), item2.toLowerCase()], combined)
+    })
+  })
+  return combined
+}
+
+module.exports = {
+  pushIfUnique,
+  ensureArray,
+  keywordCombinator,
 }
