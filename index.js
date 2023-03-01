09a48c7e481f604fa031b9c3938eab02fae8c6ed
Roga <ryanroga@gmail.com>
2023-03-01 10:11:16 -0800

    Minor Updates

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ require('colors')
 
 // EXPRESS APP
 const express = require('express')
-const App = express()
+const app = express()
 
 // MAIN ROUTER
 const MainRouter = require('./app/routes')
 
 // MIDDLEWARE
 const cors = require('cors')
-App.use(express.json())
-App.use(cors())
+app.use(express.json())
+app.use(cors())
 
 // FALLBACK ROUTE
-App.get('/', (req, res) =>
+app.get('/', (_, res) =>
   res.send(`
 <a href="/api/v1/test">/api/v1/test</a> - Test route.<br>
 <a href="/api/v1/fix">/api/v1/fix</a> - Data Fixing Route (mothballed).<br>
@@ -31,19 +31,20 @@ App.get('/', (req, res) =>
 )
 
 // MAIN ROUTES
-App.use('/', MainRouter)
+app.use('/', MainRouter)
 
 // ERROR HANDLING MIDDLEWARE
-App.use(require('./app/middleware/error.middleware.js')) // must come after other app.use statements and routes
+app.use(require('./app/middleware/error.middleware.js')) // must come after other app.use statements and routes
 
 // START SERVER
-App.listen(process.env.PORT, () => {
-  console.log('Server is', 'running'.green, 'on port', process.env.PORT.green)
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log('Server is', 'running'.green, 'on port', `${port}`.green)
   console.log(
     'Dev link',
     '(ctrl + click)'.cyan,
     '->'.magenta,
-    `http://localhost:${process.env.PORT}`.blue
+    `http://localhost:${port}`.blue
   )
   console.log(
     'Deployed link',
